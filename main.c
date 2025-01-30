4943ae09682e9ff4841dd6e25174c425235def3c
janny801 <jred8069@gmail.com>
2025-01-29 23:07:59 -0600

    added allocation function for a struct

--- main.c
+++ main.c
@@ -36,8 +36,79 @@ int main()
     scanf("%d", &age); //store age 
 
 
+   //dynamically allocate memory for a person and initialize it with user input 
+   Person *person = allocatePerson(nameBuffer, &age); 
 
+
+   //print details of the allocated person 
+   printPerson(person); 
+
+   //free dynamically allocated memory to avoid memory leaks
+   freePerson(person); 
+
+   return 0; //prog ends
+    
 
 
 
 }
+
+
+Person* allocatePerson(const char*name, const int *age)
+{
+    // allocate memory for the person structure
+    Person *newPerson = (Person*)malloc(sizeof(Person)); 
+    if(newPerson ==NULL)
+    {
+        //check if allocation failed
+        prinf("error: memory allocation failed for person"); 
+        exit(EXIT_FAILURE); 
+    }
+
+
+    //allocate memory for the name 
+    newPerson-> name= (char*)malloc((strlen(name)+1)*sizeof(char)); 
+    if(newPerson->name=NULL)
+    {
+        printf("error; memory allocation failed for name"); 
+        free(newPerson); 
+        exit(EXIT_FAILURE); 
+    }
+    strcpy(newPerson->name, name); //copy name into allocated memory
+
+
+    //allocate memory for the age 
+    newPerson->age = (int*)malloc(sizeof(int)); 
+    if(newPerson->age ==NULL)
+    {
+        prinf("error; memory allocation for age failed"); 
+        free(newPerson->name);
+        free(newPerson); 
+        exit(EXIT_FAILURE);
+    }
+    *(newPerson->age)=*age; 
+
+    return newPerson; 
+
+}
+
+
+
+
+
+
+
+
+
+/*
+notes
+strings decay to char* 
+
+each name is a string of char* in memory?
+
+dereference; access or retrieve the value stored 
+at the memory address a pointer is pointing to 
+
+
+
+*/
